298f557ac0ded974c1db9cc9b88eb40fb3d21512
Aiman Sahharon <aimansahharon44@gmail.com>
2025-01-25 14:00:53 +0800

    V0.81: Fix issue where serve level to main menu to select level to serve level doesn't load properly. Fix it by when load level set Time.timeScale=1

--- Assets/Scripts/Select Level/Open Book.cs
+++ Assets/Scripts/Select Level/Open Book.cs
@@ -1,3 +1,96 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class OpenBook : MonoBehaviour
+{
+    [SerializeField] Button openButton; //declare Open Book button
+    [SerializeField] GameObject openedBook;
+    [SerializeField] GameObject insideBackCover;
+    [SerializeField] AudioSource audioSFX; //declare audiosource
+    [SerializeField] AudioClip openBook; //declare the file to play the sound
+
+    private Vector3 rotationVector; //declare variable to rotate objects
+    private bool isOpenClicked; //boolean type variable to check the state if it has been clicked or not
+    private bool isClosedClicked;
+    private DateTime startTime; //to get start time of the animation
+    private DateTime endTime; //to get end time of the animation
+
+    void Start()
+    {
+        if (openButton != null) //if button has been assigned and once the button is clicked then call the openButton_Click() function
+        {
+            openButton.onClick.AddListener(() => openButton_Click());
+        }
+
+        AppEvents.CloseBook += new EventHandler(closeBook_Click);   
+        
+    }
+
+    void Update()
+    {
+        if (isOpenClicked || isClosedClicked) //once the user clicks the button start the animation/rotation of the object
+        {
+            transform.Rotate(rotationVector * Time.deltaTime);
+            endTime = DateTime.Now;
+
+            if (isOpenClicked)
+            {
+                if ((endTime - startTime).TotalSeconds >= 1) //after one second of rotating, then stop
+                {
+                    isOpenClicked = false;
+                    insideBackCover.SetActive(false);
+                    openedBook.SetActive(true);
+                    // Don't deactivate the book here!
+                }
+            }
+
+            if (isClosedClicked)
+            {
+                if ((endTime - startTime).TotalSeconds >= 1) //after one second of rotating, then stop
+                {
+                    isClosedClicked = false;
+                }
+            }
+        }
+    }
+
+    private void openButton_Click()
+    {
+        isOpenClicked = true;
+        startTime = DateTime.Now; //start the time
+        rotationVector = new Vector3(0, 180, 0); //rotate the object of the Y axis to 180 degree
+        PlaySound(); //As the object rotates, play a sound
+    }
+
+    private void closeBook_Click(object sender, EventArgs e)
+    {
+        if (!gameObject.activeInHierarchy) return; // Make sure the object is active
+
+        openedBook.SetActive(false); // Hide the opened book
+        insideBackCover.SetActive(true); // Show the inside back cover
+        isClosedClicked = true;
+        startTime = DateTime.Now; // Reset start time
+        rotationVector = new Vector3(0, -180, 0); // Rotate in the opposite direction
+        PlaySound();
+    }
+
+    private void PlaySound()
+    {
+        if (audioSFX != null && openBook != null) //if audioSFX and openBook have been assigned, then play the audio
+        {
+            audioSFX.PlayOneShot(openBook); //only play the sound once
+        }
+    }
+
+    void OnDestroy()
+{
+    AppEvents.CloseBook -= closeBook_Click;
+}
+} */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -26,6 +119,7 @@ public class OpenBook : MonoBehaviour
         }
 
         AppEvents.CloseBook += new EventHandler(closeBook_Click);   
+        
     }
 
     void Update()
--- Assets/Scripts/Select Level/SelectLevel.cs
+++ Assets/Scripts/Select Level/SelectLevel.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement; //to load other scene
@@ -28,4 +28,55 @@ public class SelectLevel : MonoBehaviour
     }
 
 
+} */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // To load other scenes
+
+public class SelectLevel : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        // Subscribe to the sceneLoaded event
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        // Unsubscribe from the sceneLoaded event
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // Reset time scale to 1 whenever a scene is loaded
+        Time.timeScale = 1;
+    }
+
+    public void ToMainMenu()
+    {
+        SceneManager.LoadSceneAsync("Main Menu");
+    }
+
+    public void ToNasiLemakLevel()
+    {
+        SceneManager.LoadSceneAsync("Nasi Lemak Level");
+    }
+
+    public void ToRotiCanaiLevel()
+    {
+        SceneManager.LoadSceneAsync("Roti Canai Level");
+    }
+
+    public void ToCharKwayTeowLevel()
+    {
+        SceneManager.LoadSceneAsync("Char Kway Teow Level");
+    }
+
+    public void ToServeLevel()
+    {
+        SceneManager.LoadSceneAsync("Serve Level");
+    }
 }
